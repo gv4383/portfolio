ad90cc796bc45b3c710ff2b3c9d3493df7063516
gv4383 <gregory.vargas18@gmail.com>
2020-08-26 20:56:15 -0500

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,149 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+import Gallery from '../components/Gallery'
+import Layout from '../components/layout'
+import resume from '../assets/resume/Greg_Vargas_Software_Engineer_Resume.pdf'
+
+const HomeIndex = () => {
+  const siteTitle = 'Gatsby Starter - Strata'
+  const siteDescription = 'Site description'
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>{siteTitle}</title>
+        <meta name="description" content={siteDescription} />
+      </Helmet>
+
+      <div id="main">
+        <section id="one">
+          <header className="major">
+            <h2>
+              Hey there, I'm Greg!
+            </h2>
+          </header>
+          <p>
+            I'm a software engineer with a passion for creating innovative solutions.
+            <br />
+            <br />I originally graduated with a degree in Electrical
+            Engineering from Texas A&M University, but decided to jump into
+            software development after seeing a YouTube video on the tech
+            industry in Silicon Valley. It's been a long, continuous journey
+            full of learning and I couldn't be more content with my decision
+            to venture down this path.
+            <br />
+            <br />
+            When I'm not coding, you'll find me:
+            <ul>
+              <li>
+                Playing guitar and jamming with the best musicians around
+                town!
+              </li>
+              <li>Learning new cooking recipies</li>
+              <li>Bouldering</li>
+              <li>Hiking and exploring nature</li>
+            </ul>
+            Below, you'll find my credentials as well as the accomplishments
+            I've made throughout my journey in software development.
+          </p>
+          <ul className="actions">
+            <li>
+              <a href={resume} className="button" download>
+                Download Resume
+              </a>
+            </li>
+          </ul>
+        </section>
+
+        <section id="two">
+          <h2>Recent Work</h2>
+
+          <Gallery />
+
+          <ul className="actions">
+            <li>
+              <a href="#" className="button">
+                Full Portfolio
+              </a>
+            </li>
+          </ul>
+        </section>
+
+        <section id="three">
+          <h2>Get In Touch</h2>
+          <p>
+            Accumsan pellentesque commodo blandit enim arcu non at amet id arcu
+            magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem
+            vulputate lorem neque lorem ipsum dolor.
+          </p>
+          <div className="row">
+            <div className="8u 12u$(small)">
+              <form method="post" action="#">
+                <div className="row uniform 50%">
+                  <div className="6u 12u$(xsmall)">
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      placeholder="Name"
+                    />
+                  </div>
+                  <div className="6u 12u$(xsmall)">
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div className="12u">
+                    <textarea
+                      name="message"
+                      id="message"
+                      placeholder="Message"
+                      rows="4"
+                    ></textarea>
+                  </div>
+                </div>
+                <ul className="actions" style={{ marginTop: 30 }}>
+                  <li>
+                    <input type="submit" value="Send Message" />
+                  </li>
+                </ul>
+              </form>
+            </div>
+            <div className="4u 12u$(small)">
+              <ul className="labeled-icons">
+                <li>
+                  <h3 className="icon fa-home">
+                    <span className="label">Address</span>
+                  </h3>
+                  1234 Somewhere Rd.
+                  <br />
+                  Nashville, TN 00000
+                  <br />
+                  United States
+                </li>
+                <li>
+                  <h3 className="icon fa-mobile">
+                    <span className="label">Phone</span>
+                  </h3>
+                  000-000-0000
+                </li>
+                <li>
+                  <h3 className="icon fa-envelope-o">
+                    <span className="label">Email</span>
+                  </h3>
+                  <a href="#">hello@untitled.tld</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </div>
+    </Layout>
+  )
+}
+
+export default HomeIndex
