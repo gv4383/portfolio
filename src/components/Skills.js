18a5c3c069f8072891106ec8703dd40da4a402ef
gv4383 <gregory.vargas18@gmail.com>
2020-08-30 15:25:39 -0500

    Add rails to skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,91 +1,94 @@
 import React from 'react'
 
-import icon1 from '../assets/skills/DevIcons-01.svg'
-import icon3 from '../assets/skills/DevIcons-03.svg'
-import icon4 from '../assets/skills/DevIcons-04.svg'
-import icon5 from '../assets/skills/DevIcons-05.svg'
-import icon6 from '../assets/skills/DevIcons-06.svg'
-import icon8 from '../assets/skills/DevIcons-08.svg'
-import icon9 from '../assets/skills/DevIcons-09.svg'
-import icon10 from '../assets/skills/DevIcons-10.svg'
-import icon11 from '../assets/skills/DevIcons-11.svg'
-import icon13 from '../assets/skills/DevIcons-13.svg'
-import icon14 from '../assets/skills/DevIcons-14.svg'
-import icon15 from '../assets/skills/DevIcons-15.svg'
+import reactLogo from '../assets/skills/react_logo.svg'
+import es6Logo from '../assets/skills/es6_logo.svg'
+import expressLogo from '../assets/skills/express_logo.svg'
+import vueLogo from '../assets/skills/vue_logo.svg'
+import gitLogo from '../assets/skills/git_logo.svg'
+import postgresLogo from '../assets/skills/postgres_logo.svg'
+import nodeLogo from '../assets/skills/node_logo.svg'
+import npmLogo from '../assets/skills/npm_logo.svg'
+import sassLogo from '../assets/skills/sass_logo.svg'
+import cssLogo from '../assets/skills/css_logo.svg'
+import reduxLogo from '../assets/skills/redux_logo.svg'
+import htmlLogo from '../assets/skills/html_logo.svg'
+import railsLogo from '../assets/skills/rails_logo.svg'
+// import railsLogo from '../assets/skills/rails-logo.png'
 
 const skillsArray = [
   {
     id: '1',
     name: 'React',
     url: 'https://reactjs.org/',
-    src: icon1,
+    src: reactLogo,
   },
   {
-    id: '14',
+    id: '2',
     name: 'Redux',
     url: 'https://redux.js.org/',
-    src: icon14,
-  },
-  {
-    id: '5',
-    name: 'Vue.js',
-    url: 'https://vuejs.org/',
-    src: icon5,
+    src: reduxLogo,
   },
   {
     id: '3',
-    name: 'ES6+',
-    url: 'https://es6.io/',
-    src: icon3,
+    name: 'Ruby on Rails',
+    url: 'https://rubyonrails.org/',
+    src: railsLogo,
+    // src: '',
   },
   {
     id: '4',
-    name: 'Express',
-    url: 'https://expressjs.com/',
-    src: icon4,
+    name: 'Vue.js',
+    url: 'https://vuejs.org/',
+    src: vueLogo,
   },
   {
-    id: '9',
+    id: '5',
+    name: 'ES6+',
+    url: 'https://es6.io/',
+    src: es6Logo,
+  },
+  {
+    id: '6',
+    name: 'Express',
+    url: 'https://expressjs.com/',
+    src: expressLogo,
+  },
+  {
+    id: '7',
     name: 'Node',
     url: 'https://nodejs.org/en/',
-    src: icon9,
+    src: nodeLogo,
   },
   {
     id: '8',
     name: 'PostgreSQL',
     url: 'https://www.postgresql.org/',
-    src: icon8,
+    src: postgresLogo,
   },
   {
-    id: '11',
+    id: '9',
     name: 'Sass',
     url: 'https://sass-lang.com/',
-    src: icon11,
-  },
-  {
-    id: '15',
-    name: 'HTML5',
-    url: 'https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5',
-    src: icon15,
-  },
-  {
-    id: '13',
-    name: 'CSS3',
-    url: 'https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3',
-    src: icon13,
-  },
-  {
-    id: '6',
-    name: 'Git + GitHub',
-    url: 'https://git-scm.com/',
-    src: icon6,
+    src: sassLogo,
   },
   {
     id: '10',
-    name: 'NPM',
-    url: 'https://www.npmjs.com/',
-    src: icon10,
+    name: 'HTML5',
+    url: 'https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5',
+    src: htmlLogo,
   },
+  {
+    id: '11',
+    name: 'CSS3',
+    url: 'https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3',
+    src: cssLogo,
+  },
+  {
+    id: '12',
+    name: 'Git + GitHub',
+    url: 'https://git-scm.com/',
+    src: gitLogo,
+  }
 ]
 
 const Skills = () => {
@@ -99,7 +102,13 @@ const Skills = () => {
           rel="noopener noreferrer"
         >
           <p>{skill.name}</p>
-          <img className="hvr-grow" src={skill.src} alt={skill.name} />
+          <div>
+            <img
+              className="hvr-grow"
+              src={skill.src}
+              alt={skill.name}
+            />
+          </div>
         </a>
       </div>
     )
