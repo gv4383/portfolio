2c119db775cc2f0b45481b7267283b475d68dd6a
gv4383 <gregory.vargas18@gmail.com>
2020-08-30 16:24:08 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2f77a87bf228242c02df.js"
+    "url": "webpack-runtime-e3a08820931e7398fd38.js"
   },
   {
     "url": "framework-a576ae5ab153fa4a7c27.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "65598d9cadf53dd77ce1570a31c898c6"
+    "revision": "197f624ce82b289e5749dca3531ecc77"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "e62fafbb065be49e384791506913f594"
+    "revision": "d34180a569fa0b656444b7f5b23419a6"
   },
   {
     "url": "polyfill-830cd53ca5c6720b5926.js"
