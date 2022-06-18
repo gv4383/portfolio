00646994fde72df9a19dddb4007a978dfa84b06b
gv4383 <gregory.vargas18@gmail.com>
2022-06-17 20:57:52 -0500

    Add iOS projects to personal projects section

--- src/components/Gallery/constants/images.js
+++ src/components/Gallery/constants/images.js
@@ -1,36 +1,57 @@
-import fetchTech from '../../../assets/images/projects/fetch_tech.png'
-import soundSkwer from '../../../assets/images/projects/sound_skwerl.png'
-import noDb from '../../../assets/images/projects/noDB.png'
+import recipeMe from '../../../assets/images/projects/recipe_me_app_icon.png'
+import rainBoot from '../../../assets/images/projects/rain_boot_app_icon.png'
 
 export const IMAGES = [
     {
+      id: '0',
+      source: recipeMe,
+      thumbnail: recipeMe,
+      caption: 'RecipeMe',
+      description: 'An easy way to keep track of your recipes.',
+      technology: 'iOS | Swift | SwiftUI | Core Data | Combine',
+      github: 'https://github.com/gv4383/RecipeMe',
+      link: 'https://github.com/gv4383/RecipeMe'
+    },
+    {
       id: '1',
-      source: fetchTech,
-      thumbnail: fetchTech,
-      caption: 'Fetch Tech',
-      description: 'Bringing the latest tech news to all tech enthusiasts.',
-      technology: 'React | Redux | Express | Node | PostgreSQL | Ant Design',
-      github: 'https://github.com/fetch-tech/fetch-tech',
-      link: 'https://github.com/fetch-tech/fetch-tech'
-    },
-    {
-      id: '2',
-      source: soundSkwer,
-      thumbnail: soundSkwer,
-      caption: 'Sound Skwerl',
-      description: 'Search for songs and create custom playlists.',
-      technology: 'React | Redux | Express | Node | PostgreSQL | Spotify Web API',
-      github: 'https://github.com/gv4383/project-playlist',
-      link: 'http://soundskwerl.com'
-    },
-    {
-      id: '3',
-      source: noDb,
-      thumbnail: noDb,
-      caption: 'No Database Project',
-      description: 'Customize a list of videogame heroes.',
-      technology: 'React | Express | Node | Overwatch API',
-      github: 'https://github.com/gv4383/no-db-project',
-      link: 'https://github.com/gv4383/no-db-project'
+      source: rainBoot,
+      thumbnail: rainBoot,
+      caption: 'RainBoot',
+      description: 'Track the weather in your local area.',
+      technology: 'iOS | Swift | SwiftUI | Core Location | OpenWeather API',
+      github: 'https://github.com/gv4383/RainBoot',
+      link: 'https://github.com/gv4383/RainBoot'
     }
+
+    // Web Projects
+    // {
+    //   id: '1',
+    //   source: fetchTech,
+    //   thumbnail: fetchTech,
+    //   caption: 'Fetch Tech',
+    //   description: 'Bringing the latest tech news to all tech enthusiasts.',
+    //   technology: 'React | Redux | Express | Node | PostgreSQL | Ant Design',
+    //   github: 'https://github.com/fetch-tech/fetch-tech',
+    //   link: 'https://github.com/fetch-tech/fetch-tech'
+    // },
+    // {
+    //   id: '2',
+    //   source: soundSkwer,
+    //   thumbnail: soundSkwer,
+    //   caption: 'Sound Skwerl',
+    //   description: 'Search for songs and create custom playlists.',
+    //   technology: 'React | Redux | Express | Node | PostgreSQL | Spotify Web API',
+    //   github: 'https://github.com/gv4383/project-playlist',
+    //   link: 'http://soundskwerl.com'
+    // },
+    // {
+    //   id: '3',
+    //   source: noDb,
+    //   thumbnail: noDb,
+    //   caption: 'No Database Project',
+    //   description: 'Customize a list of videogame heroes.',
+    //   technology: 'React | Express | Node | Overwatch API',
+    //   github: 'https://github.com/gv4383/no-db-project',
+    //   link: 'https://github.com/gv4383/no-db-project'
+    // }
 ]
