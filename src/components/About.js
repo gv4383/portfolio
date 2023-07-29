a1ab3eedd3fe7a7ecadb24ff6d8d1c572ec675a9
gv4383 <gregory.vargas18@gmail.com>
2023-07-29 10:09:13 -0500

    Update about section and resume

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 
-import resume from '../assets/resume/Greg_Vargas_iOS_Engineer_Resume.pdf'
+import resume from '../assets/resume/Greg_Vargas_Software_Engineer_Resume.pdf'
 
 const About = () => (
   <Fragment>
@@ -11,27 +11,18 @@ const About = () => (
       </h2>
     </header>
     <p>
-      I'm a software engineer with 4 years of professional experience and a passion for creating innovative solutions.
-      <br />
-      <br />
-      In the past, I've worked as a Front End/Full Stack Engineer, but now I'm looking to make the leap into iOS development. I believe the knowledge and experience I've gained in the last 4 years will allow me to make for a smooth transition as an iOS Engineer.
+      I'm a software engineer with 4 years of professional experience and a passion for building products and creating innovative solutions for them. My specializations are in Front End/Full Stack Web Development and iOS Development.
       <br />
       <br />
       When I'm not coding, you'll find me:
     </p>
     <ul>
-      <li>
-        Playing guitar and jamming with the best musicians around
-        town!
-      </li>
-      <li>Disc golfing</li>
+      <li>Playing guitar and jamming with the best musicians aroundtown!</li>
       <li>Dancing salsa</li>
+      <li>Disc golfing</li>
       <li>Learning new cooking recipies</li>
     </ul>
-    <p>
-      Below, you'll find my credentials as well as the accomplishments
-      I've made throughout my journey in software development.
-    </p>
+    <p>Below, you'll find my credentials as well as the accomplishments I've made throughout my journey in software development.</p>
     <ul className="actions">
       <li>
         <a href={resume} className="button" download>
